ef2e349d62d8c27cf9021654f37ce59ab2cc72c4
Saksham  Pandey <saksham4200@gmail.com>
2025-04-01 14:21:05 +0530

    css loading issue corrected

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,23 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', 
   plugins: [react()],
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith('.css')) {
+            return 'assets/css/[name].[hash].[ext]';
+          }
+          return 'assets/[name].[ext]';
+        }
+      }
+    }
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCase'
+    }
+  }
 });
