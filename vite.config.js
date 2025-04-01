b2026bb0b7ebdacb13d182106317671e64664483
Saksham  Pandey <saksham4200@gmail.com>
2025-04-01 15:07:41 +0530

    MIME Error Fixed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    assetsDir: 'assets', 
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
         assetFileNames: (assetInfo) => {
@@ -12,7 +12,7 @@ export default defineConfig({
             return 'assets/css/[name].[hash][extname]';
           }
           if (/\.(svg|png|jpg|jpeg|webp|gif)$/.test(assetInfo.name)) {
-            return 'assets/[name].[hash][extname]';
+            return 'assets/[name].[hash][extname]'; 
           }
           return 'assets/[name].[hash][extname]';
         },
