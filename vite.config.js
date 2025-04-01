143d8fd291366ca083996dd7185d0091548d69ed
Saksham  Pandey <saksham4200@gmail.com>
2025-04-01 14:52:34 +0530

    svg loading error fixed

--- vite.config.js
+++ vite.config.js
@@ -7,24 +7,23 @@ export default defineConfig({
     assetsDir: 'assets',
     rollupOptions: {
       output: {
+        // CSS files from src/styles
         assetFileNames: (assetInfo) => {
-          if (assetInfo.name.endsWith('.css')) {
-            return 'assets/css/[name].[hash].[ext]';
+          if (assetInfo.name.includes('src/styles')) {
+            return 'assets/css/[name].[hash][extname]';
           }
-          if (/\.(svg|jpg|jpeg|png|webp)$/.test(assetInfo.name)) {
-            return 'assets/images/[name].[hash].[ext]';
+          // Images from src/assets
+          if (assetInfo.name.includes('src/assets')) {
+            return 'assets/images/[name].[hash][extname]';
           }
-          return 'assets/[name].[ext]';
+          return 'assets/[name].[hash][extname]';
         }
       }
     }
   },
-  server: {
-    assetsInclude: ['**/*.svg', '**/*.webp']
-  },
   css: {
     modules: {
-      localsConvention: 'camelCase',
+      localsConvention: 'camelCase'
     }
   }
 });
