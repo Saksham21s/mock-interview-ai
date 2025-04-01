5fab4d3af5c647768c15bede7c9402e8916e6a40
Saksham  Pandey <saksham4200@gmail.com>
2025-04-01 14:09:31 +0530

    css paths added to load during runnning deployed link

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import './styles/App.min.css';
+import './styles/Interview.css';
+import './styles/resumeValidator.min.css';
+import './styles/PageNotFound.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
